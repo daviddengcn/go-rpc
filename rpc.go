0b897609567e6d0c03008cdadc3e3e7b49703339
David <david@rd.netease.com>
2013-06-25 08:36:29 +0800

    MOD some refactoring ADD some helpful functions ADD some comments

--- rpc.go
+++ rpc.go
@@ -1,32 +1,62 @@
+/*
+	Package rpc makes a RPC service through HTTP protocol.
+	Different from the net/rpc package. The methods of the serving object can be
+	of much flexible. The only requirement is the parameters and return values
+	are be represented by JSON.
+
+	Example:
+		//// Shared
+		type Arith int
+		
+		func (*Arith) Add(a, b int) int {
+			return a + b
+		}
+		
+		//// Server
+		Register(new(Arith))
+	
+		go http.ListenAndServe(":1235", nil)
+		
+		//// Client
+		rpcClient := NewClient(http.DefaultClient, "http://localhost:1235")
+	
+		A, B := 1, 2
+		var C int
+		err := rpcClient.Call(2, "Add", A, B, &C)
+*/
 package rpc
 
 import (
-	"errors"
-	"reflect"
-	"net/http"
 	"encoding/json"
-	"net/url"
-	
+	"errors"
 	"fmt"
-//	"io/ioutil"
+	"net/http"
+	"net/url"
+	"reflect"
 )
 
 type methodInfo struct {
 	funcValue reflect.Value
-	inTypes []reflect.Type
-	outTypes []reflect.Type
+	inTypes   []reflect.Type
 }
 
+/*
+	Server represents an instance for server. *Server satisfies http.Handler
+	interface.
+*/
 type Server struct {
-	oValue reflect.Value
+	oValue  reflect.Value
 	methods map[string]*methodInfo
 }
 
-const defaultRpcPath = "/_http_rpc"
+/*
+	The default path
+*/
+const DefaultPath = "/_http_rpc"
 
 const (
-	errCodeOk int = iota
-	errCodeUnknownMethod
+	errCodeOk            int = iota // Ok
+	errCodeUnknownMethod            // Unknown method name
 )
 
 type rpcResponse struct {
@@ -34,10 +64,12 @@ type rpcResponse struct {
 	Outs []string
 }
 
+/**** Server ****/
+
+// Implementation of http.Handler
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	mname := r.FormValue("method")
-fmt.Println("[Server.ServeHTTP] method:", mname)
-	
+
 	mi := s.methods[mname]
 	if mi == nil {
 		res := rpcResponse{
@@ -47,20 +79,18 @@ fmt.Println("[Server.ServeHTTP] method:", mname)
 		w.Write(replyJson)
 		return
 	}
-	
-fmt.Println("[Server.ServeHTTP] mi.inTypes:", mi.inTypes)
-	callArr := make([]reflect.Value, len(mi.inTypes) + 1) // plus 1 for the receiver
+
+	callArr := make([]reflect.Value, len(mi.inTypes)+1) // plus 1 for the receiver
 	callArr[0] = s.oValue
-	
+
 	inJsons := r.Form["in"]
-fmt.Println("[Server.ServeHTTP] inJsons:", inJsons)
 	// set parameters
 	for i := range mi.inTypes {
 		pInV := reflect.New(mi.inTypes[i])
 		json.Unmarshal([]byte(inJsons[i]), pInV.Interface())
-		callArr[i + 1] = reflect.Indirect(pInV)
+		callArr[i+1] = reflect.Indirect(pInV)
 	}
-	
+
 	outs := mi.funcValue.Call(callArr)
 
 	outJsons := make([]string, len(outs))
@@ -76,43 +106,81 @@ fmt.Println("[Server.ServeHTTP] inJsons:", inJsons)
 	w.Write(replyJson)
 }
 
-func Register(o interface{}) *Server {
-	server := &Server {
-		oValue: reflect.ValueOf(o),
+/*
+	NewServer creates a *Server instance for an object o, whose methods are
+	called for RPC service.
+*/
+func NewServer(o interface{}) *Server {
+	server := &Server{
+		oValue:  reflect.ValueOf(o),
 		methods: make(map[string]*methodInfo),
 	}
-	
+
 	oType := reflect.TypeOf(o)
-	for m := 0; m < oType.NumMethod(); m ++ {
+	for m := 0; m < oType.NumMethod(); m++ {
 		method := oType.Method(m)
 		tp := method.Type
-		
-		inTypes := make([]reflect.Type, tp.NumIn() - 1)
+
+		inTypes := make([]reflect.Type, tp.NumIn()-1)
 		for i := range inTypes {
 			inTypes[i] = tp.In(i + 1) // first is receiver, so i + 1
 		}
-		outTypes := make([]reflect.Type, tp.NumOut())
-		for i := range outTypes {
-			outTypes[i] = tp.Out(i)
-		}
-		
-		server.methods[method.Name] = &methodInfo {
+
+		server.methods[method.Name] = &methodInfo{
 			funcValue: method.Func,
-			inTypes: inTypes,
-			outTypes: outTypes,
+			inTypes:   inTypes,
 		}
 	}
-	
-	http.Handle(defaultRpcPath, server)
 	
 	return server
 }
 
-type Client struct {
-	httpClient *http.Client
-	host string
+/*
+	Register calls RegisterPath with the DefaultPath.
+*/
+func Register(o interface{}) {
+	RegisterPath(o, DefaultPath)
 }
 
+/*
+	RegisterPath registers a Server with http.Handle
+*/
+func RegisterPath(o interface{}, path string) {
+	http.Handle(path, NewServer(o))
+}
+
+/**** Client ****/
+
+/*
+	Client represents an RPC client.
+*/
+type Client struct {
+	httpClient *http.Client
+	host       string
+}
+
+/*
+	NewClient creates a *Client with DefaultPath
+*/
+func NewClient(httpClient *http.Client, host string) *Client {
+	return NewClientPath(httpClient, host, DefaultPath)
+}
+
+/*
+	NewClientPath creates a *Client with specified path.
+*/
+func NewClientPath(httpClient *http.Client, host, path string) *Client {
+	return &Client{
+		httpClient: httpClient,
+		host:       host + path,
+	}
+}
+
+/*
+	Call makes a RPC. numIn here is to distinguish the parameters(in's) from
+	return values (out's) in inPOuts. For the return values, the pointers of
+	each receiving variable is needed.
+*/
 func (c *Client) Call(numIn int, method string, inPOuts ...interface{}) error {
 	inJsons := make([]string, numIn)
 	for i := range inJsons {
@@ -120,42 +188,35 @@ func (c *Client) Call(numIn int, method string, inPOuts ...interface{}) error {
 		if err != nil {
 			return err
 		}
-		
+
 		inJsons[i] = string(inJson)
 	}
 	resp, err := c.httpClient.PostForm(c.host, url.Values{
 		"method": {method},
-		"in": inJsons,
+		"in":     inJsons,
 	})
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	
+
 	dec := json.NewDecoder(resp.Body)
 	var res rpcResponse
 	err = dec.Decode(&res)
 	if err != nil {
 		return err
 	}
-	
+
 	if res.Code != errCodeOk {
 		return errors.New(fmt.Sprintf("Error code: %s", res.Code))
 	}
-	
+
 	for i := range res.Outs {
-		err := json.Unmarshal([]byte(res.Outs[i]), inPOuts[numIn + i])
+		err := json.Unmarshal([]byte(res.Outs[i]), inPOuts[numIn+i])
 		if err != nil {
 			return err
 		}
 	}
 
 	return nil
-}
-
-func NewClient(httpClient *http.Client, host string) *Client {
-	return &Client {
-		httpClient: httpClient,
-		host: "http://" + host + defaultRpcPath,
-	}
 }
--- rpc_test.go
+++ rpc_test.go
@@ -1,8 +1,9 @@
 package rpc
 
 import (
-	"testing"
 	"net/http"
+	"testing"
+	"fmt"
 )
 
 type Arith int
@@ -12,19 +13,38 @@ func (*Arith) Add(a, b int) int {
 }
 
 func TestBasic(t *testing.T) {
-	Register(new(Arith))
-	
+	/** Server **/
+	RegisterPath(new(Arith), "/rpc")
 	go http.ListenAndServe(":1234", nil)
 	
-	rpcClient := NewClient(http.DefaultClient, "localhost:1234")
-	
+	/** Client **/
+	rpcClient := NewClientPath(http.DefaultClient, "http://localhost:1234", "/rpc")
+
 	var C int
 	err := rpcClient.Call(2, "Add", 1, 2, &C)
 	if err != nil {
 		t.Errorf("rpcClient.Call failed: %v", err)
 	}
-	
+
 	if C != 3 {
 		t.Errorf("C should be 3")
 	}
+}
+
+func ExampleArith() {
+	Register(new(Arith))
+
+	go http.ListenAndServe(":1235", nil)
+
+	rpcClient := NewClient(http.DefaultClient, "http://localhost:1235")
+
+	A, B := 1, 2
+	var C int
+	err := rpcClient.Call(2, "Add", A, B, &C)
+	if err != nil {
+		fmt.Printf("rpcClient.Call failed: %v\n", err)
+	}
+
+	fmt.Printf("%d + %d = %d\n", A, B, C)
+	// Output: 1 + 2 = 3
 }
